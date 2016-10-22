a3392a89b71e262783caaf4acdddbb2577103cee
Lukas Koch <lukas.koch@mailbox.org>
2016-10-22 22:51:50 +0200

    Improve list displays in admin interface.

--- mqtt_logger/admin.py
+++ mqtt_logger/admin.py
@@ -3,8 +3,19 @@ from django.contrib import admin
 from models import *
 
 class MessageAdmin(admin.ModelAdmin):
-    readonly_fields = ['subscription', 'time_recorded', 'topic', 'payload']
+    readonly_fields = ('subscription', 'time_recorded', 'topic', 'payload')
+    date_hierarchy = 'time_recorded'
+    list_display = ('time_recorded', 'subscription', 'topic', 'payload')
+    list_display_links = ('payload',)
+    list_filter = ('subscription', 'time_recorded', 'topic')
+    ordering = ('-time_recorded',)
 
 admin.site.register(MQTTMessage, MessageAdmin)
 
-admin.site.register(MQTTSubscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('server', 'topic', 'active')
+    list_editable = ('active',)
+    list_filter = ('server', 'active')
+    ordering = ('server', 'topic')
+
+admin.site.register(MQTTSubscription, SubscriptionAdmin)
