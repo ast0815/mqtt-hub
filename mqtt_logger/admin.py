0ccf5f03068dd17cbc4965dfedc6873476fde3d2
Lukas Koch <lukas.koch@mailbox.org>
2016-10-23 15:37:21 +0200

    Add link to subscription to MQTTMessage admin page.

--- mqtt_logger/admin.py
+++ mqtt_logger/admin.py
@@ -1,14 +1,24 @@
 from django.contrib import admin
+from django.core.urlresolvers import reverse
+from django.utils.html import format_html
 
 from .models import *
 
 class MessageAdmin(admin.ModelAdmin):
-    readonly_fields = ('subscription', 'time_recorded', 'topic', 'payload')
+    readonly_fields = ('subscription_link', 'time_recorded', 'topic', 'payload')
     date_hierarchy = 'time_recorded'
     list_display = ('time_recorded', 'subscription', 'topic', 'payload')
     list_display_links = ('payload',)
     list_filter = ('subscription', 'time_recorded', 'topic')
     ordering = ('-time_recorded',)
+
+    def subscription_link(self, obj):
+        """Return the link to the MQTTSubscription object admin page."""
+
+        url = reverse('admin:%s_%s_change' %(obj.subscription._meta.app_label,  obj.subscription._meta.model_name),  args=[obj.subscription.id])
+        return format_html('<a href="{}">{}</a>', url, str(obj.subscription))
+
+    subscription_link.short_description = "MQTT subscription"
 
 admin.site.register(MQTTMessage, MessageAdmin)
 
