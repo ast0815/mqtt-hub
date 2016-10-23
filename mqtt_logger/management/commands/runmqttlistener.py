686c4b9906948ce60711fa02f413b84450be4932
Lukas Koch <lukas.koch@mailbox.org>
2016-10-23 21:50:19 +0200

    Fix 10 second delay when starting listener.

--- mqtt_logger/management/commands/runmqttlistener.py
+++ mqtt_logger/management/commands/runmqttlistener.py
@@ -14,8 +14,8 @@ class Command(BaseCommand):
         self.stdout.write("Quit with CTRL-C.")
         try:
             while(True):
-                time.sleep(10)
                 MQTTSubscription.update_running_subscriptions()
+                time.sleep(10)
         except KeyboardInterrupt:
             self.stdout.write("Quitting...")
             MQTTSubscription.stop_running_subscriptions()
